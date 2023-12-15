0a33193a637e3dee6a3bbaac4e21287821db4bc1
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-12-15 12:25:07 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
 
+  devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
 
-  devise_for :customers
-
-
+  devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
