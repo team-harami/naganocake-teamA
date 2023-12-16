3924b7f30fd6640020899c9a4d587cc1805e2745
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-12-16 09:33:21 +0000

    　1215-2

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,6 +3,13 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+def after_sign_in_path_for(resource)
+    # 遷移先のパス
+end
+
+def after_sign_out_path_for(resource)
+
+end
   # GET /resource/sign_in
   # def new
   #   super
