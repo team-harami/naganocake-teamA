2ac5d55d73aac9b55cd1525b21811bd2e902f1ed
EC2 Default User <ec2-user@ip-172-31-46-71.ap-northeast-1.compute.internal>
2023-12-16 08:41:11 +0000

    会員のitemコントロらーとビューを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'items/index'
+  get 'items/show'
   devise_for :users
 
   root to: 'homes#top'
   get 'about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  resources :items, only: [:index, :show]
+ 
 
   namespace :admin do
     resources :genres, only:[:index, :create, :edit, :update]
