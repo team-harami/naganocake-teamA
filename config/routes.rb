3dd874c540e4ab82d772047a8cc3d0b4fac55b0c
t-akei <145541870+t-akei@users.noreply.github.com>
2023-12-18 05:36:47 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,16 +12,24 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
+ 
   scope module: :public do
     delete 'cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_cart'
     resources :items, only: [:index, :show]
     resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :orders do #only: [:new, :thanks, :create, :index, :show]
+      collection do
+        post 'confirm'
+        get 'thanks'
+      end
+    end  
   end
 
 
   namespace :admin do
     resources :genres, only:[:index, :create, :edit, :update]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
   end
 
 end
