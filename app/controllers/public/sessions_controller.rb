f1e8741c5b912a326bf57b38e6199a476a81c0c4
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-12-18 12:49:00 +0000

    public/customers/unsubscribe/withdraw完了

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+before_action :customer_state, only: [:create]
 
 def after_sign_in_path_for(resource)
   root_path
@@ -23,6 +23,17 @@ end
   #   super
   # end
 
+private
+
+def customer_state
+  customer = Customer.find_by(email: params[:customer][:email])
+  return if customer.nil?
+  if customer.is_active
+    return unless customer.valid_password?(params[:customer][:password])
+  else
+    redirect_to  new_customer_registration_path
+  end
+end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
