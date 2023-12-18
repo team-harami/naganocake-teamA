46f6bf1204304375cc534ba61d9221a79051353d
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-12-18 11:02:36 +0000

    public/customers/edit/update完了

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,14 +4,27 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to my_page_customers_path
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
 
   def withdraw
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zip_code, :address, :telephone_number, :email)
   end
 end
