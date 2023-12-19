13749c9219a7053e2cf4e0054e930974401b8b16
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-12-19 09:54:07 +0000

    admin/customers/uemura完了

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,8 +8,31 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
   end
+end
+
+private
+
+def customer_params
+  params.require(:customer).permit(
+  :last_name,
+  :first_name,
+  :last_name_kana,
+  :first_name_kana,
+  :telephone_number,
+  :postal_cord,
+  :address,
+  :email,
+  :is_active
+  )
 end
