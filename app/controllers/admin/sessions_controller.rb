ce81498f0de957499e81a3bdd9fc2a8da1deb314
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-12-23 04:26:20 +0000

    1223-2

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,36 +1,20 @@
-# frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    root_path
-    # 注文履歴一覧のページへ飛ぶ、仮でトップに設定
+    admin_root_path
   end
 
   def after_sign_out_path_for(resource)
      new_admin_session_path
   end
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+# protected
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+# def configure_permitted_parameters
+#     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
+# end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
