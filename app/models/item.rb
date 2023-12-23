b10f54bf16539503af78451faa2c6bb0fb7e6f76
EC2 Default User <ec2-user@ip-172-31-46-71.ap-northeast-1.compute.internal>
2023-12-23 09:43:18 +0000

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,12 @@ class Item < ApplicationRecord
     belongs_to :genre
     has_many :cart_items, dependent: :destroy
     has_many :order_details, dependent: :destroy
+    
+    validates :item_image, presence: true
+    validates :name, presence: true
+    validates :introduction, presence: true
+    validates :genre_id, presence: true
+    validates :price, presence: true
 
 
     # def cart_item_by?(customer)
@@ -14,6 +20,6 @@ class Item < ApplicationRecord
     end
     
     def with_tax_price
-    (price * 1.1).floor
+        (price * 1.1).floor
     end
 end
